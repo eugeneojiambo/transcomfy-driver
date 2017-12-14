d546848387e8979277f8367cd0f0ee65195f7ed9
Eugene Ojiambo <eugeneojiambo@gmail.com>
2017-12-14 11:55:56 +0300

    Added Firebase Auth and Firebase Database

--- app/src/main/java/com/transcomfy/transcomfydriver/activity/HomeActivity.java
+++ app/src/main/java/com/transcomfy/transcomfydriver/activity/HomeActivity.java
@@ -8,14 +8,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.transcomfy.transcomfydriver.R;
+import com.transcomfy.transcomfydriver.data.model.Driver;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar tbHome;
+    private TextView tvName;
+    private TextView tvNumberPlate;
+    private TextView tvSpacesAvailable;
     private View btnAvailSpace;
     private View btnPendingRequests;
+    private TextView tvPendingRequests;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,12 +38,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         tbHome = findViewById(R.id.tb_home);
+        tvName = findViewById(R.id.tv_name);
+        tvNumberPlate = findViewById(R.id.tv_number_plate);
+        tvSpacesAvailable = findViewById(R.id.tv_spaces_available);
         btnAvailSpace = findViewById(R.id.btn_avail_space);
         btnPendingRequests = findViewById(R.id.btn_pending_requests);
+        tvPendingRequests = findViewById(R.id.tv_pending_requests);
 
         setSupportActionBar(tbHome);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        setHome(); // Set up data and refresh UI for home page
+
+        // Set action in the event avail bus space is clicked
         btnAvailSpace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,6 +58,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        // Set action in the event pending requests is clicked
         btnPendingRequests.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,11 +77,49 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_log_out:
-                onBackPressed();
+                logOut();
                 return true;
             default:
                 return false;
         }
+    }
+
+    private void setHome() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        tvName.setText(auth.getCurrentUser().getEmail());
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.getReference("drivers").child(auth.getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Driver driver = dataSnapshot.getValue(Driver.class);
+                        tvName.setText(driver.getName());
+
+                        if(driver.getBus() == null) {
+                            tvNumberPlate.setText(R.string.tv_bus_allocation);
+                            tvSpacesAvailable.setText(R.string.tv_no_bus);
+                        } else {
+                            tvNumberPlate.setText(driver.getBus().getNumberPlate());
+                            tvSpacesAvailable.setText(
+                                    String.valueOf(driver.getBus().getAvailableSpace())
+                                    .concat(getString(R.string.tv_spaces_available))
+                            );
+
+                            if(dataSnapshot.child("bus").child("requests").getValue() != null) {
+                                tvPendingRequests.setText(String.valueOf(dataSnapshot.child("bus").child("requests").getChildrenCount()));
+                            }
+
+                            /*if(driver.getBus().getRequests() != null) {
+                                tvPendingRequests.setText(String.valueOf(driver.getBus().getRequests().size()));
+                            }*/
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private void availSpace() {
@@ -73,6 +130,16 @@ public class HomeActivity extends AppCompatActivity {
     private void pendingRequests() {
         Intent intent = new Intent(HomeActivity.this, PendingRequestsActivity.class);
         startActivity(intent);
+    }
+
+    private void logOut() {
+        // Get Firebase Authentication service and Sign Out user
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signOut();
+        // Go to Log in page and clear activity history
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+        HomeActivity.this.finish();
     }
 
 }
